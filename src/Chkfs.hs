7468d85d63349afea9af81196d343e77336accef
wasabi315 <wasabi.nvim.st@gmail.com>
2020-12-04 10:09:24 +0900

    Add types

--- src/Chkfs.hs
+++ src/Chkfs.hs
@@ -8,6 +8,9 @@ module Chkfs where
 
 import           Data.Binary.Get
 import qualified Data.ByteString.Lazy as BL
+import           Data.Int
+import qualified Data.Vector          as V
+import qualified Data.Vector.Unboxed  as VU
 import           Data.Word
 import           System.Exit
 import           Test.Tasty
@@ -26,6 +29,10 @@ _FSMAGIC = 0x10203040
 
 _IPB :: Word32
 _IPB = 16
+
+
+_NDIRECT :: Word32
+_NDIRECT = 12
 
 --------------------------------------------------------------------------------
 
@@ -75,6 +82,57 @@ getSuperBlock =
         <*> getWord32host
         <*> getWord32host
         <*  skip (fromIntegral $ _BSIZE - 32*8)
+
+--------------------------------------------------------------------------------
+
+data InodeBlocks = InodeBlocks
+    { inbNblocks :: {-# UNPACK #-} !Word32
+    , inbNInodes :: {-# UNPACK #-} !Word32
+    , inbDinodes :: !(V.Vector Dinode)
+    }
+
+
+getInodeBlocks :: Word32 -> Word32 -> Get InodeBlocks
+getInodeBlocks inbNblocks inbNInodes = do
+    inbDinodes <- V.replicateM (fromIntegral inbNInodes) getDinode
+    pure InodeBlocks{..}
+
+
+data FileType
+    = Unknown
+    | Dir
+    | File
+    | Dev
+    deriving (Eq, Show)
+
+
+data Dinode = Dinode
+    { dinType  :: {-# UNPACK #-} !FileType
+    , dinMajor :: {-# UNPACK #-} !Int16
+    , dinMinor :: {-# UNPACK #-} !Int16
+    , dinNlink :: {-# UNPACK #-} !Int16
+    , dinSize  :: {-# UNPACK #-} !Word32
+    , dinAddrs :: !(VU.Vector Word32)
+    }
+
+
+getDinode :: Get Dinode
+getDinode = do
+    dinMajor <- getInt16host
+    dinType <- toFileType <$> getInt16host
+    dinNlink <- getInt16host
+    dinMinor <- getInt16host
+    dinSize <- getWord32host
+    dinAddrs <- VU.replicateM (fromIntegral $ _NDIRECT + 1) getWord32host
+    pure Dinode{..}
+
+    where
+        toFileType :: Int16 -> FileType
+        toFileType = \case
+            1 -> Dir
+            2 -> File
+            3 -> Dev
+            _ -> Unknown
 
 --------------------------------------------------------------------------------
 
